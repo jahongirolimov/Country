0c28024004c2eb8f8809235edb21aff188387d23
JOHON-DEV <machoman4701@gmail.com>
2024-02-27 06:18:52 +0500

    flag page

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let searchFlag = $("#search-flag");
 let selectFlag = $("#select-flag");
 let body = $("body");
 let darkbtn = $("#dark-btn");
-
+let flag = $(".flag");
 
 
 // --------------------- functions --------------------------------
@@ -16,19 +16,20 @@ function getData(){
 }
 
 function renderFlags(data){
+    flagWrapper.innerHTML = "";
     if(data.length > 0){
         data.forEach(el => {
             let flag_card = createElement("div", "card");
+            flag_card.setAttribute("data-flag", el.name)
             flag_card.innerHTML = `
                 <img src="${el.flag}" alt="${el.name}">
-                <div class="card-body">
+                <div data-flag="${el.name}" class="card-body">
                     <h5>${el.name}</h5>
                     <p><span>Population: </span>${el.population}</p>
                     <p><span>Region: </span>${el.region}</p>
                     <p><span>Capital: </span>${el.capital}</p>
                 </div>
             `
-    
             flagWrapper.appendChild(flag_card);
         });
     }else{
@@ -42,9 +43,10 @@ function searchRenderData(data){
     if(data.length > 0){
         data.forEach(el => {
             let flag_card = createElement("div", "card");
+            flag_card.setAttribute("data-flag", el.name)
             flag_card.innerHTML = `
                 <img src="${el.flag}" alt="${el.name}">
-                <div class="card-body">
+                <div data-flag="${el.name}" class="card-body">
                     <h5>${el.name}</h5>
                     <p><span>Population: </span>${el.population}</p>
                     <p><span>Region: </span>${el.region}</p>
@@ -62,24 +64,42 @@ function searchFlags(data, searchWord){
    data.then(el => searchRenderData(el.filter(ele => ele.name.toLowerCase().includes(searchWord))));
 }
 
-function sortOptionFlag(data, name){
-    data.then(el => searchRenderData(el.filter(ele => ele.region.toLowerCase().includes(name))));
+async function sortOptionFlag(data, name){
+    flagWrapper.innerHTML = "<span class='loader'></span>";
+    try{
+        const dataRegion = await fetch(`https://restcountries.com/v2/region/${name}`);
+        const dataRegionData = await dataRegion.json();
+        renderFlags(dataRegionData);
+    }catch(e){
+        flagWrapper.innerHTML = "";
+        innerHTML = e.value;
+    }
+    
 }
 
 function darkMode(){
     body.classList.toggle("dark-Mode");
 }
 
-// function darkMode()
+flagWrapper.addEventListener("click", (element) => {
+    let flag = element.target.parentNode.getAttribute("data-flag");
+    localStorage.setItem("country", flag);
+    if (localStorage.getItem("country")){
+        window.location.href = "./flag.html"
+    }
+});
+
+
+
 
 // ------------------------ AddEvents ------------------------
-searchFlag.addEventListener("keyup", (e) =>{
-    let search = e.target.value.toLowerCase();
+searchFlag.addEventListener("keyup", (el) =>{
+    let search = el.target.value.toLowerCase();
             searchFlags(dataFlags, search);
 });
 
 selectFlag.addEventListener("change", (el) =>{
-    sortOptionFlag(dataFlags, selectFlag.value.toLowerCase());
+    sortOptionFlag(dataFlags, selectFlag.value);
 })
 
 darkbtn.addEventListener("click", () => {
@@ -90,3 +110,6 @@ darkbtn.addEventListener("click", () => {
 // -------------------------- Call back --------------------
 const dataFlags = getData();
 dataFlags.then(renderFlags);
+
+
+
